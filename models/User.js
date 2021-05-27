08b66f3b3dead9fc895e275e746707d8b8317292
TimWeyel <tweyel@gmail.com>
2021-05-27 14:38:17 -0700

    added User model

---
+++ models/User.js
@@ -0,0 +1,32 @@
+const { Schema, model } = require('mongoose');
+const { isEmail } = require('validator');
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    unique: true,
+    validate: {
+      validator: function(value) {
+        return isEmail(value);
+      },
+      message: 'Please enter a valid email address',
+    }
+  },
+  thoughts: [{ type: Schema.Types.ObjectId, ref: 'Thought' }],
+  friends: [{ type: Schema.Types.ObjectId, ref: 'User' }]
+},
+{
+  toJSON: {
+      virtuals: true
+  },
+  id: false
+});
+
+module.exports = mongoose.model('User', userSchema);
